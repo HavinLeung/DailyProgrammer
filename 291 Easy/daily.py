5ea38c00823371bc413dce1da4ae7d2ac6da75b2
havinleung <havinleung98@gmail.com>
2017-09-26 15:04:00 -0400

    initial commit

---
+++ 291 Easy/daily.py
@@ -0,0 +1,8 @@
+# https://www.reddit.com/r/dailyprogrammer/comments/5bn0b7/20161107_challenge_291_easy_goldilocks_bear/
+# Essentially, take in goldilock's weight and max temperature
+# and print out every soup that she can drink
+
+inputs = list(map(int, open('file.txt').read().replace('\n', ' ').split(' ')))
+for x in range(2, len(inputs), 2):
+    if inputs[x] > inputs[0] and inputs[x+1] < inputs[1]:
+        print(int(x/2), end=' ')
